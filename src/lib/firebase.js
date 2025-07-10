6fef264345f898f64a90226b5aa482357a985a1b
ssh-expitrans <sasha.expitrans@gmail.com>
2025-07-10 11:23:07 -0600

    fix firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,19 @@
-// lib/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxyjQmvxrHtHj24fX2lbUeCTDZNWDc3o0",
-  authDomain: "music-plat.firebaseapp.com",
-  projectId: "music-plat",
-  storageBucket: "music-plat.firebasestorage.app",
-  messagingSenderId: "288855710473",
-  appId: "1:288855710473:web:0c271189d208928e17d1d8"
+  apiKey: "AIzaSyDQI-Ao_32vAnYN67yCcKIpncJyauW1UkE",
+  authDomain: "dance-plat.firebaseapp.com",
+  projectId: "dance-plat",
+  storageBucket: "dance-plat.appspot.com", // <-- should be .appspot.com, not .app
+  messagingSenderId: "721080377792",
+  appId: "1:721080377792:web:ad977938f25563a7e0e825"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
